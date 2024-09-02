7b3e149deee879bf97892ffbd747364052d2648e
GaelGesser <gabrielhenrig@outlook.com>
2024-09-02 09:58:29 -0300

    feat: update error in text 'Under Construction' page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-zinc-950 w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-white ">Em contrução...</h1>
+      <h1 className="text-3xl font-bold text-white ">Em construção...</h1>
       <p className="absolute bottom-4 text-zinc-500">
         by Gabriel Henrique Gesser
       </p>
