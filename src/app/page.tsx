d2524bdb2ffc9bdbe9aac62ff820553d137bda9b
GaelGesser <gabrielhenrig@outlook.com>
2024-09-02 09:46:05 -0300

    feat: add 'Under Construction' page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>Olá Mundo!!</h1>;
+  return (
+    <div className="bg-zinc-950 w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold text-white ">Em contrução...</h1>
+      <p className="absolute bottom-4 text-zinc-500">
+        by Gabriel Henrique Gesser
+      </p>
+    </div>
+  );
 }
