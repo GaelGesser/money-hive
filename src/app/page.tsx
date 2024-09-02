9a6f9a4fd0880768c46667bce054d0bf542f7cf0
GaelGesser <gabrielhenrig@outlook.com>
2024-09-02 10:07:18 -0300

    feat: add ShadCN UI components and styles to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="bg-zinc-950 w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-white ">Em construção...</h1>
-      <p className="absolute bottom-4 text-zinc-500">
+    <div className=" w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Em construção...</h1>
+      <p className="absolute bottom-4 text-muted-foreground">
         by Gabriel Henrique Gesser
       </p>
     </div>
